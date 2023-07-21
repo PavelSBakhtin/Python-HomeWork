6f6bab380c9a1c5b85ad7ba90d5ea3de7617b028
PavelSB <1987PavelS@mail.ru>
2023-07-21 19:50:00 +0300

    home work 2 task 2 done

--- home_work_2/homework2_task2.py
+++ home_work_2/homework2_task2.py
@@ -4,3 +4,27 @@
 n = int(input('Enter the valid number: '))
 h = ("%#x" % n)
 print(h)
+
+def num_to_hex(num: int) -> str:
+    result = ''
+    hexer = 16
+    while num >= 1:
+        res = num % hexer
+        if hexer == 16:
+            if res == 10:
+                res = 'a'
+            if res == 11:
+                res = 'b'
+            if res == 12:
+                res = 'c'
+            if res == 13:
+                res = 'd'
+            if res == 14:
+                res = 'e'
+            if res == 15:
+                res = 'f'
+        result += str(res)
+        num = num // hexer
+    return result[::-1]
+
+print(num_to_hex(int(input('Enter the valid number: '))), f"assert: {hex(29)}")
