dfda56789e610a60c03ff5c3f4a65521fc2edfab
PavelSB <1987PavelS@mail.ru>
2023-07-22 16:19:45 +0300

    home work 4 task 2 done

--- home_work_4/homework4_task2.py
+++ home_work_4/homework4_task2.py
@@ -2,4 +2,11 @@
 # где ключ — значение переданного аргумента, а значение — имя аргумента.
 # Если ключ не хешируем, используйте его строковое представление.
 
+def dict_create():
+    def dict_trans(**arg):
+        return {i if i.__hash__ is not None else str(i): j for j, i in arg.items()}
+    dict_insert = {'12': (1, 10), '45': 889, 'rand': ['jpg', 'pdf'], '754': 'anything'}
+    print('\nСловарь исходный:', dict_insert, sep='\n')
+    print('\nСловарь транспонированный:', dict_trans(**dict_insert), sep='\n')
 
+dict_create()
