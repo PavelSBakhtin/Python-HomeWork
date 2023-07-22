1a720a6a3d59c755cb261a22bb60f2fc106a3d11
PavelSB <1987PavelS@mail.ru>
2023-07-22 15:40:42 +0300

    home work 4 task 1 done

--- home_work_4/homework4_task1.py
+++ home_work_4/homework4_task1.py
@@ -1,3 +1,17 @@
 # Напишите функцию для транспонирования матрицы.
 
+from random import randint
 
+def matrix_create():
+    row = int(input('Enter number of rows: '))
+    col = int(input('Enter number of columns: '))
+    matrix = list([[randint(10, 99) for i in range(col)] for j in range(row)])
+    print('\nМатрица исходная:', *matrix, sep='\n')
+    return matrix
+
+def matrix_trans(matrix):
+    transpose = list(map(list, zip(*matrix)))
+    print('\nМатрица транспонированная:', *transpose, sep='\n')
+    # return transpose
+
+matrix_trans(matrix_create())
