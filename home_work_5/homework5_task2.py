60632d8b7a0fc394fd9cb6fa25bb213e335c5989
PavelSB <1987PavelS@mail.ru>
2023-07-22 19:11:11 +0300

    home work 5 task 2 done

--- home_work_5/homework5_task2.py
+++ home_work_5/homework5_task2.py
@@ -3,4 +3,12 @@
 # В результате получаем словарь с именем в качестве ключа и суммой премии в качестве значения.
 # Сумма рассчитывается как ставка умноженная на процент премии.
 
+def generate_dict(names, pays, percents):
+    return {name: pay * (1 + float(percent.strip('%')) / 100) for name, pay, percent in zip(names, pays, percents)}
 
+names = ['Иван', "Петр", "Михаил"]
+pays = [10000, 15000, 20000]
+percents = ['50.25%', "20%", "30.6%"]
+
+grants_dict = generate_dict(names, pays, percents)
+print(grants_dict)
