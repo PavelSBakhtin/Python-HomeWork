0c456896f890e8f58096c1c406f0b38338ada667
PavelSB <1987PavelS@mail.ru>
2023-07-22 18:35:13 +0300

    home work 5 task 1 done

--- home_work_5/homework5_task1.py
+++ home_work_5/homework5_task1.py
@@ -1,11 +1,23 @@
 # Напишите функцию, которая принимает на вход строку — абсолютный путь до файла.
 # Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
 
+import os
+
 def file_info(strg) -> tuple:
-    *way, file = strg.split("\\")
-    full_way = "\\".join(way) + "\\"
+    way = os.path.dirname(adress)
+    file = os.path.basename(adress)
     name, exten = file.split(".")
-    return full_way, name, exten
+    return way, name, exten
 
 adress = 'D:\учёба\Python\Python.jpg'
 print(file_info(adress))
+
+# # Первое решение, без библиотеки os:
+# def file_info(strg) -> tuple:
+#     *way, file = strg.split("\\")
+#     full_way = "\\".join(way) + "\\"
+#     name, exten = file.split(".")
+#     return full_way, name, exten
+
+# adress = 'D:\учёба\Python\Python.jpg'
+# print(file_info(adress))
