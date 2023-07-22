71b4f83a4c0101589b0606631a1c5598f110ce40
PavelSB <1987PavelS@mail.ru>
2023-07-22 18:54:02 +0300

    home work 5 task 1 done

--- home_work_5/homework5_task1.py
+++ home_work_5/homework5_task1.py
@@ -3,14 +3,25 @@
 
 import os
 
+# Если надо расширение с точкой:
 def file_info(strg) -> tuple:
-    way = os.path.dirname(adress)
-    file = os.path.basename(adress)
-    name, exten = file.split(".")
+    full_way, exten = os.path.splitext(strg)
+    way, name = os.path.split(full_way)
     return way, name, exten
 
 adress = 'D:\учёба\Python\Python.jpg'
 print(file_info(adress))
+
+# # Второе решение, с библиотекой os,
+# # расширение без точки:
+# def file_info(strg) -> tuple:
+#     way = os.path.dirname(adress)
+#     file = os.path.basename(adress)
+#     name, exten = file.split(".")
+#     return way, name, exten
+
+# adress = 'D:\учёба\Python\Python.jpg'
+# print(file_info(adress))
 
 # # Первое решение, без библиотеки os:
 # def file_info(strg) -> tuple:
@@ -19,5 +30,14 @@ print(file_info(adress))
 #     name, exten = file.split(".")
 #     return full_way, name, exten
 
-# adress = 'D:\учёба\Python\Python.jpg'
-# print(file_info(adress))
+# # Для себя:
+# #
+# # def file_info(strg) -> tuple:
+# #     return way, name, exten
+# #
+# # или
+# #
+# # def file_info(strg):
+# #     return (way, name, exten)
+# #
+# # если преобразование в кортеж не указано в имени функции, то указываем это в return
