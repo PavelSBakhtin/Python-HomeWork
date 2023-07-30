8278269d4b0a198521929ac958346dd92b7a4fee
PavelSB <1987PavelS@mail.ru>
2023-07-30 12:26:44 +0300

    home work 6 task 1 done

--- home_work_6/homework6_task1.py
+++ home_work_6/homework6_task1.py
@@ -1,3 +1,4 @@
 # В модуль с проверкой даты добавьте возможность запуска в терминале с передачей даты на проверку.
 
-
+print('the task fully completed')
+print('proof: https://github.com/PavelSBakhtin/Python-Lessons/blob/master/Lesson_6/workhome6_task1.py')
