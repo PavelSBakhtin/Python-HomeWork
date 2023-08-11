00e08ef9ea6f24dd9d1c8b3564f4742b1d344c1e
PavelSB <1987PavelS@mail.ru>
2023-08-11 15:34:31 +0300

    home work 8 task 3 changed

--- home_work_8/homework8_modules/__init__.py
+++ home_work_8/homework8_modules/__init__.py
@@ -1,4 +1,4 @@
-from .file_trans import txt_to_json, json_to_csv, csv_to_json, level_json
+from .files_trans import txt_to_json, json_to_csv, csv_to_json, level_json
 from .files_trans import json_to_pickle, pickle_to_csv, csv_to_pickle
 from .dir_walker import directory_walker
 
--- home_work_8/homework8_task3/homework8_modules/__init__.py
+++
@@ -1,7 +0,0 @@
-from .file_trans import txt_to_json, json_to_csv, csv_to_json, level_json
-from .files_trans import json_to_pickle, pickle_to_csv, csv_to_pickle
-from .dir_walker import directory_walker
-
-__all__ = ['txt_to_json', 'json_to_csv', 'csv_to_json', 'level_json',
-           'json_to_pickle', 'pickle_to_csv', 'csv_to_pickle',
-           'directory_walker']
