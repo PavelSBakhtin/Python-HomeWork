5de78aef2ec72e164e9461d4460f32d8d89e54a9
PavelSB <1987PavelS@mail.ru>
2023-08-25 12:06:59 +0300

    home work 12 task 1 done

--- home_work_12/homework12_task1.py
+++ home_work_12/homework12_task1.py
@@ -1,3 +1,3 @@
-# Решить задачи, которые не успели решить на семинаре.
+# Решите задачи, которые не успели решить на семинаре.
 
 print('tasks completed, check here: https://github.com/PavelSBakhtin/Python-Lessons/tree/master/Lesson_12')
