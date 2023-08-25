b748f869c1bc4e9557ebcb3cffd266481881a4bc
PavelSB <1987PavelS@mail.ru>
2023-08-25 11:54:09 +0300

    home work 12 task 2 done

--- home_work_12/homework12_task2.py
+++ home_work_12/homework12_task2.py
@@ -33,7 +33,7 @@ class Student:
     first_name = Descriptor()
     second_name = Descriptor()
     
-    def __init__(self, surname, first_name, second_name, csv_file='homework12_task2.csv'):
+    def __init__(self, surname, first_name, second_name, csv_file='home_work_12/homework12_task2.csv'):
         self.surname = surname
         self.first_name = first_name
         self.second_name = second_name
@@ -44,7 +44,7 @@ class Student:
             self.subjects = {row[0]: {'grade': [], 'test_score': []} for row in reader}
 
     def __str__(self):
-        return f'Студент: ФИО = {self.surname} {self.first_name} {self.second_name}'
+        return f'Студент: {self.surname} {self.first_name} {self.second_name}'
 
     def grade_add(self, subject, grade):
         if subject not in self.subjects:
@@ -71,6 +71,43 @@ class Student:
         return sum(total_score) / len(total_score) if total_score else 0
 
 
-surname = Student.surname(input('Введите фамилию: '))
-first_name = Student.first_name(input('Введите имя: '))
-second_name = Student.second_name(input('Введите отчество: '))
+# student = Student(input('Введите фамилию: '), input('Введите имя: '), input('Введите отчество: '))
+student = Student('Яков', 'Гиви', 'Окувич')
+
+subject = 'Русский язык'
+student.grade_add(subject, 3)
+student.test_score_add(subject, 57)
+student.test_score_add(subject, 66)
+print(f'{student} - {subject}, средний балл тестов = {student.test_score_average(subject)}')
+
+subject = 'Математика'
+student.grade_add(subject, 5)
+student.test_score_add(subject, 99)
+student.test_score_add(subject, 88)
+print(f'{student} - {subject}, средний балл тестов = {student.test_score_average(subject)}')
+
+subject = 'Литература'
+student.grade_add(subject, 4)
+student.test_score_add(subject, 84)
+student.test_score_add(subject, 89)
+print(f'{student} - {subject}, средний балл тестов = {student.test_score_average(subject)}')
+
+subject = 'Биология'
+student.grade_add(subject, 3)
+student.test_score_add(subject, 79)
+student.test_score_add(subject, 85)
+print(f'{student} - {subject}, средний балл тестов = {student.test_score_average(subject)}')
+
+subject = 'Физика'
+student.grade_add(subject, 5)
+student.test_score_add(subject, 96)
+student.test_score_add(subject, 87)
+print(f'{student} - {subject}, средний балл тестов = {student.test_score_average(subject)}')
+
+subject = 'Химия'
+student.grade_add(subject, 4)
+student.test_score_add(subject, 98)
+student.test_score_add(subject, 91)
+print(f'{student} - {subject}, средний балл тестов = {student.test_score_average(subject)}')
+
+print(f'{student} - средний балл по всем предметам = {student.grade_average()}')
